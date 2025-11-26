f9026fa44c90bff9634a454d870c23f731505530
WitheringAway <124115470+notwithering@users.noreply.github.com>
2025-11-26 12:37:36 -0500

    remove debug print in safeFetch

--- src/tools/utils/utils.ts
+++ src/tools/utils/utils.ts
@@ -1,5 +1,4 @@
 export async function safeFetch(url: string): Promise<Response> {
-	console.log(url);
 	const resp = await fetch(url);
 	if (!resp.ok) {
 		throw new Error(`Request failed (${resp.status} ${resp.statusText})`);
